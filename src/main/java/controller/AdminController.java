50cba6ee602f03685dbb303fd267a1fd97d31138
admin <17835353475@163.com>
2020-08-12 08:56:35 +0800

    啊啊啊

--- src/main/java/controller/AdminController.java
+++ src/main/java/controller/AdminController.java
@@ -17,6 +17,7 @@ public class AdminController {
         System.out.println(admin);
         Admin adm=adminService.query(admin);
         if(adm!=null){
+            System.out.println(adm);
             return true;
         }else{
             return false;
